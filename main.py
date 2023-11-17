fd459cb813c25f7834c68b5f0a948d895354f891
tom <wyrwa.t@gmail.com>
2023-11-17 17:17:56 +0100

    final touch

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ with open("birthdays.csv") as data_file:
             with open(f"letter_templates/letter_{rand_letter}.txt") as letter:
                 letter_txt = letter.read()
                 birthday_letter = letter_txt.replace("[NAME]", name)
-                with smtplib.SMTP("smtp.gmail.com") as connection:
-                    connection.starttls()
-                    connection.login(user=my_email, password=password)
-                    connection.sendmail(
-                        from_addr=my_email,
-                        to_addrs="aby977016@yahoo.com",
-                        msg=f"Subject: HAPPY BIRTHDAY!!! \n\n {birthday_letter}"
-                    )
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=password)
+                connection.sendmail(
+                    from_addr=my_email,
+                    to_addrs="aby977016@yahoo.com",
+                    msg=f"Subject: HAPPY BIRTHDAY!!! \n\n {birthday_letter}"
+                )
